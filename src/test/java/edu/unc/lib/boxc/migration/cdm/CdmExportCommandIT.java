ab4aa2e38d4ed49cd78af9886440b5a68b7c4863
Ben Pennell <ben.pennell@gmail.com>
2021-07-08 14:23:49 -0400

    Add command for exporting records from a collection (#2)

---
+++ src/test/java/edu/unc/lib/boxc/migration/cdm/CdmExportCommandIT.java
@@ -0,0 +1,162 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.boxc.migration.cdm;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import edu.unc.lib.boxc.migration.cdm.model.CdmFieldInfo;
+import edu.unc.lib.boxc.migration.cdm.model.CdmFieldInfo.CdmFieldEntry;
+import edu.unc.lib.boxc.migration.cdm.model.MigrationProject;
+import edu.unc.lib.boxc.migration.cdm.services.CdmFieldService;
+import edu.unc.lib.boxc.migration.cdm.services.MigrationProjectFactory;
+
+/**
+ * @author bbpennel
+ */
+public class CdmExportCommandIT extends AbstractCommandIT {
+
+    private final static String COLLECTION_ID = "my_coll";
+    private final static String PASSWORD = "supersecret";
+    private final static String BODY_RESP = "<xml>record</xml>";
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(options().dynamicPort());
+
+    private CdmFieldService fieldService;
+    private String cdmBaseUrl;
+
+    @Before
+    public void setUp() throws Exception {
+        fieldService = new CdmFieldService();
+        cdmBaseUrl = "http://localhost:" + wireMockRule.port();
+
+        stubFor(post(urlEqualTo("/cgi-bin/admin/exportxml.exe"))
+                .willReturn(aResponse()
+                        .withStatus(200)));
+        stubFor(get(urlEqualTo("/cgi-bin/admin/getfile.exe?CISOMODE=1&CISOFILE=/my_coll/index/description/export.xml"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/octet-stream")
+                        .withBody(BODY_RESP)));
+    }
+
+    @Test
+    public void exportValidProjectTest() throws Exception {
+        Path projPath = createProject();
+
+        String[] args = new String[] {
+                "-w", projPath.toString(),
+                "export",
+                "--cdm-url", cdmBaseUrl,
+                "-p", PASSWORD };
+        executeExpectSuccess(args);
+
+        MigrationProject project = MigrationProjectFactory.loadMigrationProject(projPath);
+
+        assertTrue("Export folder not created", Files.exists(project.getExportPath()));
+        assertEquals(BODY_RESP, FileUtils.readFileToString(
+                project.getExportPath().resolve("export_all.xml").toFile(), StandardCharsets.UTF_8));
+    }
+
+    @Test
+    public void noUsernameTest() throws Exception {
+        System.clearProperty("user.name");
+        Path projPath = createProject();
+
+        String[] args = new String[] {
+                "-w", projPath.toString(),
+                "export",
+                "--cdm-url", cdmBaseUrl,
+                "-p", PASSWORD };
+        executeExpectFailure(args);
+
+        MigrationProject project = MigrationProjectFactory.loadMigrationProject(projPath);
+        assertFalse("Description folder should not be created", Files.exists(project.getExportPath()));
+        assertTrue(output.contains("Must provided a CDM username"));
+    }
+
+    @Test
+    public void noPasswordTest() throws Exception {
+        Path projPath = createProject();
+
+        String[] args = new String[] {
+                "-w", projPath.toString(),
+                "export",
+                "--cdm-url", cdmBaseUrl };
+        executeExpectFailure(args);
+
+        MigrationProject project = MigrationProjectFactory.loadMigrationProject(projPath);
+        assertFalse("Description folder should not be created", Files.exists(project.getExportPath()));
+        assertTrue(output.contains("Must provided a CDM password"));
+    }
+
+    @Test
+    public void errorResponseTest() throws Exception {
+        stubFor(post(urlEqualTo("/cgi-bin/admin/exportxml.exe"))
+                .willReturn(aResponse()
+                        .withStatus(400)));
+
+        Path projPath = createProject();
+
+        String[] args = new String[] {
+                "-w", projPath.toString(),
+                "export",
+                "--cdm-url", cdmBaseUrl,
+                "-p", PASSWORD };
+        executeExpectFailure(args);
+
+        MigrationProject project = MigrationProjectFactory.loadMigrationProject(projPath);
+
+        assertFalse("Description folder should not be created", Files.exists(project.getExportPath()));
+        assertTrue(output.contains("Failed to request export"));
+    }
+
+    private Path createProject() throws Exception {
+        MigrationProject project = MigrationProjectFactory.createMigrationProject(
+                baseDir, COLLECTION_ID, null, USERNAME);
+        CdmFieldInfo fieldInfo = new CdmFieldInfo();
+        CdmFieldEntry fieldEntry = new CdmFieldEntry();
+        fieldEntry.setNickName("title");
+        fieldEntry.setExportAs("titla");
+        fieldEntry.setDescription("Title");
+        fieldInfo.getFields().add(fieldEntry);
+        CdmFieldEntry fieldEntry2 = new CdmFieldEntry();
+        fieldEntry2.setNickName("title2");
+        fieldEntry2.setExportAs("titlb");
+        fieldEntry2.setDescription("Another Title");
+        fieldInfo.getFields().add(fieldEntry2);
+        fieldService.persistFieldsToProject(project, fieldInfo);
+        return project.getProjectPath();
+    }
+}
