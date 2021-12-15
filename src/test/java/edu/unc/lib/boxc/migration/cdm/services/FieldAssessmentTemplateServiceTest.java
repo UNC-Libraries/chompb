adf261079a1313204a85b56a29b35ac6d555ff19
krwong <krwong@email.unc.edu>
2021-12-15 10:41:32 -0600

    add FieldAssessmentTemplateService, tests, and command to generate template

---
+++ src/test/java/edu/unc/lib/boxc/migration/cdm/services/FieldAssessmentTemplateServiceTest.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.boxc.migration.cdm.services;
+
+import edu.unc.lib.boxc.migration.cdm.exceptions.InvalidProjectStateException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
+
+import edu.unc.lib.boxc.migration.cdm.model.MigrationProject;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author krwong
+ */
+public class FieldAssessmentTemplateServiceTest {
+    private static final String PROJECT_NAME = "gilmer";
+    @Rule
+    public final TemporaryFolder tmpFolder = new TemporaryFolder();
+
+    private MigrationProject project;
+    private CdmFieldService fieldService;
+    private FieldAssessmentTemplateService service;
+
+    @Before
+    public void setup() throws Exception {
+        tmpFolder.create();
+        project = MigrationProjectFactory.createMigrationProject(
+                tmpFolder.getRoot().toPath(), PROJECT_NAME, null, "user");
+        fieldService = new CdmFieldService();
+
+        service = new FieldAssessmentTemplateService();
+        service.setCdmFieldService(fieldService);
+    }
+
+    // test that all fields are filled in
+    // test that all the expected rows are present (including configured and reserved)
+    // test that ignored fields don't show up in the spreadsheet
+    @Test
+    public void allExpectedCellsPopulatedTest() throws Exception {
+        populateFieldInfo();
+        service.generate(project);
+
+        Path projPath = project.getProjectPath();
+        InputStream inputStream = Files.newInputStream(projPath.resolve("field_assessment_gilmer.xlsx"));
+        XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+        Sheet sheet = workbook.getSheetAt(0);
+
+        assertEquals(60, sheet.getLastRowNum());
+        assertEquals(11, sheet.getRow(0).getPhysicalNumberOfCells());
+        assertEquals(7, sheet.getRow(1).getPhysicalNumberOfCells());
+        assertEquals(7, sheet.getRow(60).getPhysicalNumberOfCells());
+        assertNull(sheet.getRow(1).getCell(2));
+        assertNull(sheet.getRow(1).getCell(8));
+        assertNull(sheet.getRow(1).getCell(9));
+        assertNull(sheet.getRow(1).getCell(10));
+        assertNotEquals("Search by Decade", workbook.getSheetAt(0).getRow(7).getCell(0));
+        assertNotEquals("search", workbook.getSheetAt(0).getRow(7).getCell(1));
+    }
+
+    @Test
+    public void regenerateTemplateTest() throws Exception {
+        populateFieldInfo();
+        service.generate(project);
+        service.generate(project);
+
+        Path projPath = project.getProjectPath();
+        Path newPath = projPath.resolve("field_assessment_gilmer.xlsx");
+
+        assertTrue(Files.exists(newPath));
+    }
+
+    @Test
+    public void invalidFieldsTest() throws Exception {
+        try {
+            service.generate(project);
+            fail();
+        } catch (InvalidProjectStateException e) {
+            assertTrue(e.getMessage().contains("CDM fields file is missing"));
+        }
+    }
+
+    private void populateFieldInfo() throws Exception {
+        Files.copy(Paths.get("src/test/resources/gilmer_fields.csv"), project.getFieldsPath());
+    }
+}
