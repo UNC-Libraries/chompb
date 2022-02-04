79d35d9ee5504779fac45cc99d1bcb0bb0fc44d9
Sharon Luong <snluong@email.lib.unc.edu>
2022-02-04 13:11:48 -0500

    BXC-3436 first pass at logic for adding collection-specific row to redirect mapping csv

--- src/main/java/edu/unc/lib/boxc/migration/cdm/services/RedirectMappingService.java
+++ src/main/java/edu/unc/lib/boxc/migration/cdm/services/RedirectMappingService.java
@@ -15,14 +15,22 @@
  */
 package edu.unc.lib.boxc.migration.cdm.services;
 
+import static edu.unc.lib.boxc.migration.cdm.util.CLIConstants.outputLogger;
 import edu.unc.lib.boxc.migration.cdm.exceptions.MigrationException;
 import edu.unc.lib.boxc.migration.cdm.model.MigrationProject;
+import edu.unc.lib.boxc.migration.cdm.model.MigrationSip;
+import edu.unc.lib.boxc.model.api.ids.PID;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.MissingFormatWidthException;
+import java.util.Set;
 
 /**
  * Service for generating a redirect mapping csv with the following information:
@@ -32,6 +40,7 @@ import java.nio.file.Files;
  */
 public class RedirectMappingService {
     private MigrationProject project;
+    private String cdmCollectionId;
     private CSVPrinter csvPrinter;
     public static final String[] CSV_HEADERS = new String[] {
             "cdm_collection_id", "cdm_object_id", "boxc_object_id", "boxc_file_id" };
@@ -45,6 +54,7 @@ public class RedirectMappingService {
      */
     public void init() {
         try {
+            cdmCollectionId = project.getProjectProperties().getCdmCollectionId();
             BufferedWriter writer = Files.newBufferedWriter(project.getRedirectMappingPath());
             csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader(CSV_HEADERS));
         } catch (IOException e) {
@@ -67,15 +77,44 @@ public class RedirectMappingService {
     /**
      * Adds row to Redirect Mapping CSV in the correct order
      * @param cdmObjectId
-     * @param boxcWorkId
+     * @param boxcObjectId
      * @param boxcFileId
      */
-    public void addRow(String cdmObjectId, String boxcWorkId, String boxcFileId) {
-        String cdmCollectionId = project.getProjectProperties().getCdmCollectionId();
+    public void addRow(String cdmObjectId, String boxcObjectId, String boxcFileId) {
         try {
-            csvPrinter.printRecord(cdmCollectionId, cdmObjectId, boxcWorkId, boxcFileId);
+            csvPrinter.printRecord(cdmCollectionId, cdmObjectId, boxcObjectId, boxcFileId);
         } catch (IOException e) {
             throw new MigrationException("Error adding row to redirect mapping CSV", e);
         }
+    }
+
+    /**
+     * Analyzes list of sips to add collection-specific row(s) to Redirect Mapping CSV
+     * @param sips list of MigrationSips
+     */
+    public void addCollectionRow(List<MigrationSip> sips) throws IOException {
+        List<PID> destinationPids = new ArrayList<>();
+        List<PID> newCollectionPids = new ArrayList<>();
+
+        for (MigrationSip sip: sips) {
+            destinationPids.add(sip.getDestinationPid());
+            newCollectionPids.add(sip.getNewCollectionPid());
+        }
+
+        Set<PID> uniqueDestinationPids = new HashSet<>(destinationPids);
+        Set<PID> uniqueNewCollectionPids = new HashSet<>(newCollectionPids);
+
+        // there is more than one destination, so no redirect
+        if (uniqueDestinationPids.size() > 1) {
+            outputLogger.info("There were multiple possible destinations, so chompb can't select one " +
+                    "for redirect mapping. You will need to add any destinations to destinations.csv");
+        }
+        // there is exactly one distinct new collection, so we'll redirect to it
+        if (uniqueNewCollectionPids.size() == 1) {
+            csvPrinter.printRecord(cdmCollectionId, null, newCollectionPids.get(0), null);
+            return;
+        }
+        // there are 0 collections or there are multiple collections, so redirect to the boxc destination
+        csvPrinter.printRecord(cdmCollectionId, null, destinationPids.get(0), null);
     }
 }
