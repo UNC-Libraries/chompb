c0bdfebe880a5f58e6c9263759474aa5c59c44fb
krwong <krwong@email.unc.edu>
2022-05-05 14:07:41 -0400

    create FieldUrlAssessmentService and test

---
+++ src/main/java/edu/unc/lib/boxc/migration/cdm/services/FieldUrlAssessmentService.java
@@ -0,0 +1,164 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.boxc.migration.cdm.services;
+
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.unc.lib.boxc.migration.cdm.model.MigrationProject;
+import edu.unc.lib.boxc.migration.cdm.exceptions.MigrationException;
+import edu.unc.lib.boxc.migration.cdm.model.CdmFieldInfo;
+
+/**
+ * Service for validating cdm field urls report
+ * @author krwong
+ */
+public class FieldUrlAssessmentService {
+    private static final Logger log = LoggerFactory.getLogger(FieldUrlAssessmentService.class);
+
+    private MigrationProject project;
+    private CdmFieldService cdmFieldService;
+    private CdmIndexService indexService;
+
+    // header: field, url, error, successful, redirect, redirect url
+    public static final String NICK_FIELD = "cdm_nick";
+    public static final String URL = "url";
+    public static final String ERROR_INDICATOR = "error?";
+    public static final String SUCCESSFUL_INDICATOR = "successful?";
+    public static final String REDIRECT_INDICATOR = "redirect?";
+    public static final String REDIRECT_URL = "redirect url";
+    public static final String[] URL_CSV_HEADERS = new String[] {
+            NICK_FIELD, URL, ERROR_INDICATOR, SUCCESSFUL_INDICATOR, REDIRECT_INDICATOR, REDIRECT_URL };
+
+    /**
+     * List all fields and urls that were exported
+     * @throws IOException
+     */
+    //TODO: have this method return object with the field and url instead of list of urls
+    public List<String> dbFieldUrls(MigrationProject project) throws IOException {
+        indexService.setProject(project);
+        cdmFieldService.validateFieldsFile(project);
+        CdmFieldInfo fieldInfo = cdmFieldService.loadFieldsFromProject(project);
+        List<String> exportFields = fieldInfo.listAllExportFields();
+
+        List<String> listFieldUrls = new ArrayList<>();
+        Connection conn = null;
+
+        try {
+            conn = indexService.openDbConnection();
+            Statement stmt = conn.createStatement();
+            for (String field : exportFields) {
+                ResultSet rs = stmt.executeQuery(" select distinct " + field
+                        + " from " + CdmIndexService.TB_NAME
+                        + " where " + field + " like " + "'%http%'");
+                while (rs.next()) {
+                    listFieldUrls.add(rs.getString(1));
+                }
+            }
+        } catch (SQLException e) {
+            throw new MigrationException("Failed to generate field urls", e);
+        } finally {
+            CdmIndexService.closeDbConnection(conn);
+        }
+        return listFieldUrls;
+    }
+
+    /**
+     * Extracts urls from each field
+     * @throws IOException
+     */
+    public String extractUrls(String string) throws IOException {
+        String regex = "\\b((?:https?|ftp|file):" + "//[-a-zA-Z0-9+&@#/%?=" + "~_|!:, .;]*[-a-zA-Z0-9+"
+                + "&@#/%=~_|])";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(string);
+        String extractUrl = null;
+        if (matcher.find()) {
+            extractUrl = matcher.group();
+        }
+        return extractUrl;
+    }
+
+    /**
+     * Validate urls, list redirect urls, and write to csv file
+     * @throws IOException
+     */
+    public void validateUrls() throws IOException {
+        List<String> listUrls = dbFieldUrls(project);
+
+        //Path projPath = project.getProjectPath();
+        String filename = project.getProjectProperties().getName() + "_field_urls.csv";
+        //BufferedWriter writer = Files.newBufferedWriter(projPath.resolve(filename));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+        CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
+                .withHeader(URL_CSV_HEADERS));
+        var httpClient = HttpClients.createDefault();
+
+        // order of y/n: error, successful, redirect, redirect URL
+        for (String urlField : listUrls) {
+            String url = extractUrls(urlField);
+            HttpGet getMethod = new HttpGet(url);
+            try (CloseableHttpResponse resp = httpClient.execute(getMethod)) {
+                int status = resp.getStatusLine().getStatusCode();
+                if (status >= 200 && status < 300) {
+                    String success = "n,y,n, ";
+                    csvPrinter.printRecord(url, success);
+                } else if (status >= 300 && status < 400) {
+                    String redirect = "n,y,y,";
+                    String redirectUrl = resp.getFirstHeader("Location").getValue();
+                    csvPrinter.printRecord(url, redirect, redirectUrl);
+                } else if (status >= 400) {
+                    String error = "y,n,n, ";
+                    csvPrinter.printRecord(url, error);
+                } else {
+                    throw new IOException("");
+                }
+            } catch (IOException e) {
+                log.error("Failed to retrieve url: {}", url, e);
+            }
+        }
+        writer.close();
+    }
+
+    public void setProject(MigrationProject project) {
+        this.project = project;
+    }
+
+    public void setCdmFieldService(CdmFieldService cdmFieldService) {
+        this.cdmFieldService = cdmFieldService;
+    }
+
+    public void setIndexService(CdmIndexService indexService) {
+        this.indexService = indexService;
+    }
+}
---
+++ src/test/java/edu/unc/lib/boxc/migration/cdm/services/FieldUrlAssessmentServiceTest.java
@@ -0,0 +1,199 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.boxc.migration.cdm.services;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import edu.unc.lib.boxc.migration.cdm.model.MigrationProject;
+
+/**
+ * @author krwong
+ */
+public class FieldUrlAssessmentServiceTest {
+    private static final String PROJECT_NAME = "gilmer";
+
+    @Rule
+    public final TemporaryFolder tmpFolder = new TemporaryFolder();
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(options().dynamicPort());
+
+    private MigrationProject project;
+    private FieldUrlAssessmentService service;
+    private CdmFieldService fieldService;
+    private CdmIndexService indexService;
+    private String cdmBaseUrl;
+
+    @Before
+    public void setup() throws Exception {
+        project = MigrationProjectFactory.createMigrationProject(
+                tmpFolder.getRoot().toPath(), PROJECT_NAME, null, "user");
+        Files.createDirectories(project.getExportPath());
+
+        fieldService = new CdmFieldService();
+        indexService = new CdmIndexService();
+        indexService.setProject(project);
+        indexService.setFieldService(fieldService);
+        service = new FieldUrlAssessmentService();
+        service.setProject(project);
+        service.setCdmFieldService(fieldService);
+        service.setIndexService(indexService);
+
+        cdmBaseUrl = "http://localhost:" + wireMockRule.port();
+    }
+
+    @Test
+    public void retrieveCdmUrlsTest() throws Exception {
+        indexExportSamples();
+
+        List<String> listUrls = service.dbFieldUrls(project);
+        System.out.println("List of urls: " + listUrls);
+
+        assertEquals(6,listUrls.size());
+    }
+
+    @Test
+    public void successfulUrlsTest() throws Exception {
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/00276/"))
+                .willReturn(aResponse()
+                        .withStatus(200)));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/28"))
+                .willReturn(aResponse()
+                        .withStatus(200)));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/29"))
+                .willReturn(aResponse()
+                        .withStatus(200)));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/25"))
+                .willReturn(aResponse()
+                        .withStatus(200)));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/26"))
+                .willReturn(aResponse()
+                        .withStatus(200)));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/27"))
+                .willReturn(aResponse()
+                        .withStatus(200)));
+
+        indexExportSamples();
+        service.validateUrls();
+    }
+
+    @Test
+    public void redirectUrlsTest() throws Exception {
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/00276/"))
+                .willReturn(aResponse()
+                        .withStatus(300)
+                        .withHeader("Location", "https://library.unc.edu/")));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/28"))
+                .willReturn(aResponse()
+                        .withStatus(300)
+                        .withHeader("Location", "https://library.unc.edu/")));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/29"))
+                .willReturn(aResponse()
+                        .withStatus(300)
+                        .withHeader("Location", "https://library.unc.edu/")));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/25"))
+                .willReturn(aResponse()
+                        .withStatus(300)
+                        .withHeader("Location", "https://library.unc.edu/")));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/26"))
+                .willReturn(aResponse()
+                        .withStatus(300)
+                        .withHeader("Location", "https://library.unc.edu/")));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/27"))
+                .willReturn(aResponse()
+                        .withStatus(300)
+                        .withHeader("Location", "https://library.unc.edu/")));
+
+        indexExportSamples();
+        service.validateUrls();
+    }
+
+    @Test
+    public void errorUrlsTest() throws Exception {
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/00276/"))
+                .willReturn(aResponse()
+                        .withStatus(400)));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/28"))
+                .willReturn(aResponse()
+                        .withStatus(400)));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/29"))
+                .willReturn(aResponse()
+                        .withStatus(400)));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/25"))
+                .willReturn(aResponse()
+                        .withStatus(400)));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/26"))
+                .willReturn(aResponse()
+                        .withStatus(400)));
+        stubFor(get(urlEqualTo( cdmBaseUrl + "/cdm/ref/collection/gilmer/id/27"))
+                .willReturn(aResponse()
+                        .withStatus(400)));
+
+        indexExportSamples();
+        service.validateUrls();
+    }
+
+    @Test
+    public void expectedCsv() throws Exception {
+
+    }
+
+    @Test
+    public void regenerateCsv() throws Exception {
+        indexExportSamples();
+
+        service.validateUrls();
+        service.validateUrls();
+
+        Path projPath = project.getProjectPath();
+        Path newPath = projPath.resolve("gilmer_field_urls.csv");
+
+        assertTrue(Files.exists(newPath));
+    }
+
+    private void indexExportSamples() throws Exception {
+        Files.copy(Paths.get("src/test/resources/sample_exports/export_1.xml"),
+                project.getExportPath().resolve("export_1.xml"));
+        Files.copy(Paths.get("src/test/resources/sample_exports/export_2.xml"),
+                project.getExportPath().resolve("export_2.xml"));
+        Files.copy(Paths.get("src/test/resources/gilmer_fields.csv"), project.getFieldsPath());
+
+        project.getProjectProperties().setExportedDate(Instant.now());
+        indexService.createDatabase(false);
+        indexService.indexAll();
+    }
+}
