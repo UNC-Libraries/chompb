83ae2366d350b2f29cafa76a9ebc7b9ab71c5867
Sharon Luong <snluong@email.lib.unc.edu>
2022-05-27 10:28:48 -0400

    remove import

--- src/main/java/edu/unc/lib/boxc/migration/cdm/services/FieldUrlAssessmentService.java
+++ src/main/java/edu/unc/lib/boxc/migration/cdm/services/FieldUrlAssessmentService.java
@@ -35,7 +35,6 @@ import org.apache.commons.csv.CSVPrinter;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
