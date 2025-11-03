5fbb71756bcf5b8aa185ebf9646451ae4111fe28
sharonl <6546457+sharonluong@users.noreply.github.com>
2025-11-03 16:06:45 -0500

    Bxc 5483 expand bug (#138)
    * BXC-5483 clarify logic
    
    * BXC-5483 update logic

--- src/main/java/edu/unc/lib/boxc/migration/cdm/services/DescriptionsService.java
+++ src/main/java/edu/unc/lib/boxc/migration/cdm/services/DescriptionsService.java
@@ -220,8 +220,11 @@ public class DescriptionsService {
             return false;
         }
         Attribute labelAttr = element.getAttributeByName(DISPLAY_LABEL_NAME);
-        return labelAttr != null && (CDM_ID_LABEL.equals(labelAttr.getValue()) ||
-                CHOMPB_ID_LABEL.equals((labelAttr.getValue())));
+        if (labelAttr == null) {
+            return false;
+        }
+        var label = labelAttr.getValue();
+        return CDM_ID_LABEL.equals(label) || CHOMPB_ID_LABEL.equals(label);
     }
 
     /**
